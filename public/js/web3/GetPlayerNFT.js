f9dffe9b58955136e94862929c265486ce625a86
Aleksey Bykhun <theisease@gmail.com>
2022-04-10 17:09:11 +0800

    fix

--- public/js/web3/GetPlayerNFT.js
+++ public/js/web3/GetPlayerNFT.js
@@ -2,7 +2,7 @@ export async function getPlayerNFT(moralis) {
     const playerAddress = '0xeac41D05531770b85ad1E0f145b94BFE205bDa78'
     const result = await moralis.Web3.getNFTs({chain: 'eth', address: playerAddress, limit: '10'});
     console.log(result.length);
-    const promises = result.map((r) => {
+    const promises = result.map(async (r) => {
         if (r.token_uri) {
             let url = fixURL(r.token_uri);
             try {
@@ -11,8 +11,6 @@ export async function getPlayerNFT(moralis) {
                 let data
                 if (url.startsWith("data:application/json")) {
                     data = await fetch(url).then(r => r.json());
-                } else if (true) {
-                    data = await moralis.Web3.getMetadata({chain: 'eth', uri: url});
                 } else {
                     const proxiedURL = `/metadata?uri=${encodeURIComponent(url)}`;
                     data = await fetch(proxiedURL).then(r => r.json());
