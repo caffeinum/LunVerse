b6e622ee5617b8563362b14f9bf2f61b934d628b
Aleksey Bykhun <theisease@gmail.com>
2022-04-11 14:47:56 +0800

    draft

--- public/js/web3/GetPlayerNFT.js
+++ public/js/web3/GetPlayerNFT.js
@@ -1,12 +1,15 @@
 import { sceneEvents } from "../Events/EventsCenter";
 
+const IPFS_GATEWAY = 'https://buildship.mypinata.cloud/ipfs/';
+
 export async function getPlayerNFT(moralis) {
     const playerAddress1 = '0xeac41D05531770b85ad1E0f145b94BFE205bDa78';
     const playerAddress2 = '0xffE06cb4807917bd79382981f23d16A70C102c3B';
     const result = await moralis.Web3.getNFTs({ chain: 'eth', address: playerAddress2 });
 
-    var pageResults = [];
-    var currentPage;
+    const pageResults = [];
+    let currentPage;
+
     sceneEvents.on('getNFTsFromPage', async (page) => {
         currentPage = page;
 
@@ -15,45 +18,63 @@ export async function getPlayerNFT(moralis) {
             return;
         }
 
-        let count = 0;
-        const promises = result.map(async (r) => {
-            if ((page - 1) * 12 <= count && count < page * 12) {
-                count++;
-                if (r.token_uri) {
-                    let url = fixURL(r.token_uri);
-                    try {
-                        // if url is data:application/json; fetch right away
-                        // if not, proxy via /metadata?uri=
-                        let data
-                        if (url.startsWith("data:application/json")) {
-                            data = await fetch(url).then(r => r.json());
-                        } else if (false) {
-                            data = await moralis.Web3.getMetadata({ chain: 'eth', uri: url });
-                        } else {
-                            const proxiedURL = `/metadata?uri=${encodeURIComponent(url)}`;
-                            data = await fetch(proxiedURL).then(r => { return r.json() });
-                        }
-    
-                        if (data && data.image) {
-                            return { image: fixImageURL(data.image), name: data.name };
-                        }
-    
-                    } catch (err) {
-                        console.log('Error with', r.token_uri, err);
+        let index = 0;
+
+        const metadatas = result.map(async (r) => {
+            index++;
+
+            const isPage = (page - 1) * 12 <= index && index < page * 12
+
+            if (isPage) {
+                // if (r.metadata) {
+                //     const m = JSON.parse(r.metadata)
+
+                //     if (m.image) {
+                //         return { image: fixImageURL(m.image), name: m.name };
+                //     }
+                // }
+
+                const { token_address, token_id } = r
+
+                const token = await moralis.Web3API.token.getTokenIdMetadata({ chain: 'eth', address: token_address, token_id });
+
+                console.log('r', r)
+                console.log('token', token)
+
+                const { metadata } = token
+
+                if (metadata) {
+                    const m = JSON.parse(metadata)
+
+                    if (m.image) {
+                        return { image: fixImageURL(m.image), name: m.name };
                     }
                 }
-            } else {
-                count++;
-                return null;
+
+                console.log('wrong metadata', token.name, token.token_uri, metadata);
+
+                // if (!r.token_uri) {
+                //     // TODO: return "not loaded" image object
+                //     return
+                // }
+
+                // try {
+                //     return getMetadata(r.token_uri);
+                // }  catch (err) {
+                //     console.log('Error with', r.token_uri, err);
+                // }
             }
         })
-        Promise.all(promises).then(r => {
-            r = r.filter(data => data != null);
-            if (currentPage > pageResults.length) {
-                pageResults.push(r);
-            }
-            sceneEvents.emit('getNFTsFromPageResult', r);
-        });
+
+        const r = await Promise.all(metadatas);
+
+        const pageResult = r.filter(data => data != null);
+
+        if (currentPage > pageResults.length) {
+            pageResults.push(pageResult);
+        }
+
+        sceneEvents.emit('getNFTsFromPageResult', pageResult);
     });
 
     sceneEvents.emit('makeNFTsPanel', result.length);
@@ -62,7 +83,8 @@ export async function getPlayerNFT(moralis) {
 
 function fixURL(url) {
     if (url.startsWith("ipfs")) {
-        return "https://ipfs.moralis.io:2053/ipfs/" + url.split("ipfs://ipfs/").slice(-1)[0];
+        const [, hash] = url.split("ipfs://ipfs/");
+        return IPFS_GATEWAY + hash;
     }
     else {
         return url;
@@ -70,8 +92,32 @@ function fixURL(url) {
 }
 function fixImageURL(url) {
     if (url.startsWith("ipfs")) {
-        return "https://ipfs.moralis.io:2053/ipfs/" + url.split("ipfs://").slice(-1)[0];
+        const [ , hash ] = url.split("ipfs://");
+        return IPFS_GATEWAY + hash;
     } else {
         return url;
     }
+}
+
+const getMetadata = async (uri) => {
+    let url = fixURL(uri);
+
+    // if url is data:application/json; fetch right away
+    // if not, proxy via /metadata?uri=
+    let data
+    if (url.startsWith("data:application/json")) {
+        data = await fetch(url).then(r => r.json());
+    } else if (false) {
+        data = await moralis.Web3.getMetadata({ chain: 'eth', uri: url });
+    } else {
+        const proxiedURL = `/metadata?uri=${encodeURIComponent(url)}`;
+        data = await fetch(proxiedURL).then(r => { return r.json() });
+    }
+
+    if (data && data.image) {
+        return { image: fixImageURL(data.image), name: data.name };
+    }
+
+    return null
+
 }
